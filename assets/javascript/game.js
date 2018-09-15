b57bbd8ed92b43b4982a406df195b36e9153ee20
Nicole W <Nicole.Wilkins2018@gmail.com>
2018-09-15 18:21:59 -0400

    game functions hurray!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 console.log("game.js loaded")
 
+$(document).ready(function(){
+
 var counter = 0;
 var win = 0;
 var loses = 0;
@@ -7,68 +9,91 @@ var loses = 0;
 //random number displayed to user between 19-120
 var targetNumber = Math.floor(Math.random() * 99) + 19;
 //when crystal clicked add specific number to players total score number between 1-12
-var numberOptions = Math.floor(Math.random() * 12) +1;
+var numberOptions1 = Math.floor(Math.random() * 12) +1;
+var numberOptions2 = Math.floor(Math.random() * 12) +1;
+var numberOptions3 = Math.floor(Math.random() * 12) +1;
+var numberOptions4 = Math.floor(Math.random() * 12) +1;
 var whiteCrystal = $("#whiteCrystal");
 var purpleCrystal = $("#purpleCrystal");
-
-var NewCrystalValue = whiteCrystalValue + purpleCrystalValue
-
-$(document).ready(function(){
+var tealCrystal = $("#tealCrystal");
+var orangeCrystal = $("#orangeCrystal");
 
 $("#randomNumber").text(targetNumber);
 
 //numberOptions value given to whiteCrystal
-whiteCrystal.attr("data-whitecrystalvalue", numberOptions);
+whiteCrystal.attr("data-whitecrystalvalue", numberOptions1);
 
 $("#whiteCrystal").on("click", function() {
     var whiteCrystalValue = ($(this).attr("data-whitecrystalvalue"));
     whiteCrystalValue = parseInt(whiteCrystalValue);
     counter += whiteCrystalValue;
     console.log("white crystal value" + whiteCrystalValue)
-
-        //player wins when total score matches random number from beginng of game
-       if (counter === targetNumber) {
-            $("#wins").text(wins+1);
-            alert("you win!");      
-         }
-
-         //player loses when total score is abover random number
-        else if(counter >= targetNumber) {
-             $("#loses").text(loses+1);
-             alert("you lose!");
-         }
-
-        $("#user-Score").text(counter);
-        console.log(counter)
-
+    totalScore();
 })
 
 //numberOptions value given to purpleCrystal
-purpleCrystal.attr("data-purplecrystalvalue", numberOptions);
+purpleCrystal.attr("data-purplecrystalvalue", numberOptions2);
 
-$("purpleCrystal").on("click", function(){
+$("#purpleCrystal").on("click", function() {
     var purpleCrystalValue = $(this).attr("data-purplecrystalvalue");
     purpleCrystalValue = parseInt(purpleCrystalValue);
     counter += purpleCrystalValue;
     console.log("purple crystal value" + purpleCrystalValue)
+    totalScore();
 })
-//new fuction for score
+
+//numberOptions value given to tealCrystal
+tealCrystal.attr("data-tealcrystalvalue", numberOptions3);
+
+$("#tealCrystal").on("click", function() {
+    var tealCrystalValue = $(this).attr("data-tealcrystalvalue");
+    tealCrystalValue = parseInt(tealCrystalValue);
+    counter += tealCrystalValue;
+    console.log("teal crystal value" + tealCrystalValue)
+    totalScore();
+})
+
+//numberOptions value given to orangeCrystal
+orangeCrystal.attr("data-orangecrystalvalue", numberOptions4);
+
+$("#orangeCrystal").on("click", function() {
+    var orangeCrystalValue = $(this).attr("data-orangecrystalvalue");
+    orangeCrystalValue = parseInt(orangeCrystalValue);
+    counter += orangeCrystalValue;
+    console.log("orange crystal value" + orangeCrystalValue)
+    totalScore();
+})
+    
+// for score
+var totalScore = function () {
     if(counter === targetNumber) {
+        wins++
         $("#wins").text(wins+1);
         alert("you win!");
+        reset();
     }
 
     else if(counter >= targetNumber) {
+        loses++
         $("#loses").text(loses+1);
         alert("you lose!");
+        reset();
     }
 
     $("#user-Score").text(counter);
+    console.log(counter)
+    }
 
-    //reset game but keep score
+//reset game
+var reset = function() {
+    counter = 0
+    targetNumber = Math.floor(Math.random() * 99) + 19;
+    $("user-Score").text(counter);
+    $("#randomNumber").text(targetNumber);
+}
+
+//closes document on ready
 })
-
-var NewCrystalValue = whiteCrystalValue + purpleCrystalValue
 
     //when crystal clicked update players score counter
 //game restarts when player wins or loses
